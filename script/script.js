75c81d4da43e6305b087ab68b1587ecc68ad2fd9
Ursu98 <ursuandrei5985@gmail.com>
2022-10-27 10:17:40 +0300

    new calc

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const value1 = +prompt('Brat first number please')
+const value2 = +prompt('Brat second number please')
+const operation = prompt('Brat input operation please')
+
+const h1 = document.querySelector('h1')
+if (operation === '+'){
+h1.textContent = value1 + value2
+}else if (operation === '-'){
+    h1.textContent = value1 - value2
+}else if (operation === '*'){
+    h1.textContent = value1 * value2
+}else if (operation === '/'){
+    h1.textContent = value1 / value2
+}else{
+    h1.textContent = 'Brat sheva ne to'
+}
